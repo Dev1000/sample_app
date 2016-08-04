3115bca5523b7ea9c66b48bcd6b45a72bb298931
Ed R <eduardorams@yahoo.com>
2016-08-03 22:03:18 -0500

    Finish Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # return the full title
+  def full_title(page_title = '')
+    base_title = 'Rails 5.0.0'
+
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
+
 end
